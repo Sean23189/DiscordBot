08d4e7190b9f9765cc357be8ee3d674b8b93e2bc
Sean <66129659+Sean23189@users.noreply.github.com>
2023-04-04 11:30:14 +0000

    Improved readability

--- extensions/google.py
+++ extensions/google.py
@@ -3,25 +3,25 @@ import requests
 import lightbulb
 
 # get API key and custom search engine ID from environment variables
-API_KEY = os.getenv("GOOGLEAPIKEY")
-CX = os.getenv("CUSTOMSEARCHENGINEID")
+API_KEY = os.getenv('GOOGLEAPIKEY')
+CX = os.getenv('CUSTOMSEARCHENGINEID')
 
 # create a new Lightbulb plugin for the Google search command
-plugin = lightbulb.Plugin("google")
+plugin = lightbulb.Plugin('google')
 
 # function that takes a search query and returns the top search result
 def get_top_result(query: str) -> str:
-    url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={CX}&q={query}"
+    url = f'https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={CX}&q={query}'
     # add timeout of 5 seconds to requests.get to prevent the program from hanging indefinitely
     response = requests.get(url, timeout=5)
     json_data = response.json()
-    top_result = json_data['items'][0]['title'] + "\n" + json_data['items'][0]['link']
+    top_result = json_data['items'][0]['title'] + '\n' + json_data['items'][0]['link']
     return top_result
 
 # define a Lightbulb command that takes a search query and responds with the top search result
 @plugin.command
-@lightbulb.option("query", "search query", type=str)
-@lightbulb.command("google", "search for anything on google")
+@lightbulb.option('query', 'search query', type=str)
+@lightbulb.command('google', 'search for anything on google')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def google_cmd(ctx: lightbulb.Context):
     query = ctx.options.query
--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import os  # For retrieving environment variables
 import lightbulb  # For creating the Discord bot instance
 
 bot = lightbulb.BotApp(
-    token=os.getenv("DISCORDBOTTOKEN"), # Retrieve the Discord bot token
-    logs="DEBUG" # Set the logging level to debug
+    token=os.getenv('DISCORDBOTTOKEN'), # Retrieve the Discord bot token
+    logs='DEBUG' # Set the logging level to debug
 )
 
-# Load all extensions from the "./extensions" directory
-bot.load_extensions_from("./extensions")
+# Load all extensions from the './extensions' directory
+bot.load_extensions_from('./extensions')
 
 # Start the bot
 bot.run()
