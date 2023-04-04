84fb3a9d1c37ac5bec4c2b78cc2b8606a36a2790
Sean <66129659+Sean23189@users.noreply.github.com>
2023-04-04 12:01:07 +0000

    Now uses Sqlite instead of a standard local file

--- extensions/userinput.py
+++ extensions/userinput.py
@@ -1,8 +1,20 @@
+import sqlite3
 import lightbulb
 import hikari
 
 # Define a plugin with the name 'userinput'
 plugin = lightbulb.Plugin('userinput')
+
+# Connect to the SQLite database
+connection = sqlite3.connect('userinput.db')
+cursor = connection.cursor()
+
+# Create the userinput table if it does not exist
+cursor.execute('''CREATE TABLE IF NOT EXISTS userinput
+                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   user TEXT,
+                   message TEXT)''')
+connection.commit()
 
 # Define a command called 'userinput' which takes an argument called 'input_text'
 @plugin.command
@@ -17,56 +29,65 @@ async def store_input(ctx: lightbulb.Context):
     # Get the 'input_text' option passed in by the user
     input_text = ctx.options.input_text
 
-    # Write the username, discriminator, and input text to a file
-    with open('userinput.txt', 'a', encoding='utf-8') as file:
-        file.write(f'{fullname}: {input_text}\n')
+    # Insert the username, discriminator, and input text into the userinput table
+    cursor.execute('INSERT INTO userinput (user, message) VALUES (?, ?)', (fullname, input_text))
+    connection.commit()
 
-    # Get the line number of the newly added message
-    with open('userinput.txt', 'r', encoding='utf-8') as file:
-        line_number = len(file.readlines())
+    # Get the row number of the newly added message
+    row_number = cursor.lastrowid
 
     # Respond to the user to confirm that their message was stored
-    await ctx.respond(f'Stored message {input_text} for user {fullname} on line {line_number}.')
+    await ctx.respond(f'Stored message {input_text} for user {fullname} on row {row_number}.')
 
 # Define a command called 'searchinput' which takes an argument called 'search'
 @plugin.command
-@lightbulb.option('search', 'The line number to search for', int)
-@lightbulb.command('searchinput', 'Search for a line in the userinput file')
+@lightbulb.option('search', 'The row number to search for', int)
+@lightbulb.command('searchinput', 'Search for a row in the userinput table')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def search_input(ctx: lightbulb.Context):
     # Get the 'search' option passed in by the user
-    search_line_number = ctx.options.search
+    search_row_number = ctx.options.search
 
-    # Read the lines from the userinput file
-    with open('userinput.txt', 'r', encoding='utf-8') as file:
-        lines = file.readlines()
+    fullname = ctx.author.username + '#' + ctx.author.discriminator
 
-    # Check if the requested line number exists in the file
-    if len(lines) < search_line_number:
-        await ctx.respond('That line does not exist.')
+    # Select the row from the userinput table
+    cursor.execute('SELECT message FROM userinput WHERE id = ?', (search_row_number,))
+    result = cursor.fetchone()
+
+    # Check if the requested row exists in the table
+    if not result:
+        await ctx.respond('That row does not exist.')
     else:
         # Get the requested message and respond to the user with it
-        message = lines[search_line_number - 1].strip()
-        await ctx.respond(message)
+        message = result[0]
+        await ctx.respond(fullname + ": " + message)
 
 @plugin.command()
-@lightbulb.command('showall', 'Show all entries from the userinput list!')
+@lightbulb.command('showall', 'Show all entries from the userinput table!')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def showall(ctx):
-    # Open the userinput.txt file in read mode
-    with open('userinput.txt', 'r', encoding='utf-8') as file:
-        # Create a new embed
-        embed = hikari.Embed(
-            title='All Entries to the userinput file!',
-            color=hikari.Color.from_rgb(255, 0, 0)
-        )
-        # Read each line and add it as a field to the embed
-        line_num = 0
-        for line in file:
-            line_num += 1
-            embed.add_field(name=('Entry N.' + str(line_num)), value=line.strip())
+    # Select all rows from the userinput table
+    cursor.execute('SELECT * FROM userinput')
+    results = cursor.fetchall()
+
+    fullname = ctx.author.username + '#' + ctx.author.discriminator
+
+    # Create a new embed
+    embed = hikari.Embed(
+        title='All Entries to the userinput table!',
+        color=hikari.Color.from_rgb(255, 0, 0)
+    )
+
+    # Add each row as a field to the embed
+    for row in results:
+        embed.add_field(name=('Row N.' + str(row[0])), value=fullname + ": " + row[2])
+
     # Send the embed
     await ctx.respond(embed=embed)
+
+# Define a function to unload the plugin from the bot
+def unload(bot):
+    connection.close()
 
 
 # Define a function to load the plugin into the bot
